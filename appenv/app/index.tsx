b63256a8418fcf96291140486eeef0a786923043
5yndr0m <sdilanjana21@gmail.com>
2024-12-17 23:26:15 +0530

    style: adding some styling

--- appenv/app/index.tsx
+++ appenv/app/index.tsx
@@ -1,4 +1,4 @@
-import { Image, Text, View } from "react-native";
+import { Image, Text, View, StyleSheet } from "react-native";
 
 export default function Index() {
   return (
@@ -10,8 +10,23 @@ export default function Index() {
       }}
     >
       <Image source={require("../assets/images/react-logo.png")} style={{ width: 200, height: 200 }} />
-      <Text>Hello World!</Text>
-      <Text>Welcome to my app!</Text>
+      <Text style={styles.titleText} >Hello World!</Text>
+      <Text style={styles.paraText}>Welcome to my app!</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  titleText: {
+    fontSize: 32,
+    textAlign: "center",
+    color: 'aqua',
+    fontWeight: '800',
+  },
+  paraText: {
+    fontSize: 16,
+    textAlign: "center",
+    color: '#000',
+    fontWeight: '500',
+  },
+});
