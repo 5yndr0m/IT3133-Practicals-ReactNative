bd185c6d1780879a45abc83b2f8aa956f706a4d8
5yndr0m <sdilanjana21@gmail.com>
2024-12-22 20:40:58 +0530

    refactor: changing project structure
    removed the old typescript base project and opting into using blank
    javascript project

---
+++ appenv/App.js
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, View } from 'react-native';
+import { PaperProvider } from 'react-native-paper';
+import Home from './components/Home';
+
+export default function App() {
+  return (
+    <PaperProvider>
+    <View style={styles.container}>
+      <Home />
+      <StatusBar style="auto" />
+    </View>
+    </PaperProvider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
+});
