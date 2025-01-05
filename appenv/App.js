cc8c0824b58d30e0471708cb4de6357ac409d236
5yndr0m <sdilanjana21@gmail.com>
2025-01-05 16:32:08 +0530

    feat: setting up navigation

--- appenv/App.js
+++ appenv/App.js
@@ -1,32 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View, ScrollView, KeyboardAvoidingView, Platform } from 'react-native';
-import { PaperProvider } from 'react-native-paper';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import Home from './components/Home';
 import ContactUs from './components/ContactUs';
+import AboutUs from './components/AboutUs';
 
+const Stack = createNativeStackNavigator();
 export default function App() {
+  const Stack = createNativeStackNavigator();
   return (
-    <PaperProvider>
-      <SafeAreaView>
-        <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
-          <ScrollView>
-            <View style={styles.container}>
-              <ContactUs />
-              <StatusBar style="auto" />
-            </View>
-          </ScrollView>
-        </KeyboardAvoidingView>
-      </SafeAreaView>
-    </PaperProvider>
+
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName='home'
+          screenOptions={{
+            headerStyle: { backgroundColor: '#e2bee2' },
+          }}
+        >
+          <Stack.Screen name='home' component={Home} options={{ title: "Welcome" }} />
+          <Stack.Screen name='contact' component={ContactUs} options={{ title: "Contact Us" }} />
+          <Stack.Screen name='aboutus' component={AboutUs} options={{ title: "About Us" }} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
+
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  }
-});
